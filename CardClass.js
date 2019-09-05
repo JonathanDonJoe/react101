ad1f82a139ac025c9486a7806fa63822dc0c4048
Jonathan Joe <JonathanDonJoe@gmail.com>
2019-09-05 11:36:39 -0400

    Reorganized repo 2

--- CardClass.js
+++
@@ -1,9 +0,0 @@
-class CardClass extends React.Component{
-    constructor() {
-        super()
-    }
-    // Every class component MUST have a render method
-    render() {
-        return <h1> Sanity Check </h1>
-    }
-}
